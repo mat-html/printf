794f6c15323cf3126f06d9a52ba0da4565bec467
Jovan Matic <jomatic@student.42vienna.com>
2026-05-29 16:44:01 +0200

    check_arguments function inside of ft_printf.c done

--- ft_printf.c
+++ ft_printf.c
@@ -6,10 +6,33 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 14:00:30 by jomatic           #+#    #+#             */
-/*   Updated: 2026/05/29 15:46:43 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/05/29 16:43:23 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	check_args(const char *input, void *args)
+{
+	int	i;
+
+	i = 0;
+	if (input == 'c')
+		i += print_char((int)args);
+	else if (input = 's')
+		i += print_string((char *)args);
+	else if (input = 'p')
+		i += print_adress(args);
+	else if (input = 'd')
+		i += print_numb((int)args);
+	else if (input = 'i')
+		i += print_uns_numb((int)args);
+	else if (input = 'u')
+		i += print_int((unsigned int)args);
+	else if (input = 'x')
+		i += print_hex((int)args);
+	else if (input = 'X')
+		i += print_hex((int)args);
+	return (i);
+}
 int	ft_printf(const char *input, ...)
 {
 	va_list	args;
@@ -23,7 +46,7 @@ int	ft_printf(const char *input, ...)
 		{
 			input++;
 			if (ft_strchr("cspdiuxX", *input))
-				i += check_arg(*input, va_args(arg, void *));
+				i += check_arg(*input, va_args(args, void *));
 			else if (*input == '%')
 				i += print_char(input, 1);
 		}
